2f7f6513c5a39aa584e342ac63a08f40f78a2dbe
FLERES13 <juniorfleres@gmail.com>
2025-08-05 21:24:20 -0300

    Primeiro commit do projeto de orientação a objetos

---
+++ ORIENTACAO_A_OBJETOS/03_herenca/classe.py
@@ -0,0 +1,41 @@
+
+class Pessoa:
+
+    def __init__(self, telefone, endereco):
+        self.telefone = telefone
+        self.endereco = endereco
+
+    def exibir_dados(self):
+        print(f"Telefone: {self.telefone}")
+        print(f"Endereco: {self.endereco}")    
+        
+class PessoaFisica(Pessoa):
+    
+    def __init__(self, nome, cpf, telefone, endereco):
+        self.nome = nome
+        self.cpf = cpf 
+        super().__init__(telefone, endereco)
+
+    def exibir_dados(self):
+          print(f"Nome: {self.nome}")
+          print(f"CPF: {self.cpf}")
+          super().exibir_dados()     
+
+
+class PessoaJuridica(Pessoa):   
+
+    def __init__(self, nome_fantasia, cnpj, telefone, endereco):
+        self.nome_fantasia = nome_fantasia
+        self.cnpj = cnpj
+        super().__init__(telefone, endereco)    
+
+
+    def exibir_dados(self):
+          print(f"Nome da empresa : {self.nome_fantasia}")
+          print(f"Cnpj: {self.cnpj}")
+          super().exibir_dados()     
+        
+
+
+     
+
